76c97f04b398f43fb48e6c441866f9c7f12a9f74
DSmith575 <SMITDE5@student.op.ac.nz>
2023-08-26 22:25:16 +1200

    build(tailwind): added tailwind themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,24 @@ module.exports = {
 	],
 	theme: {
 		extend: {
+			colors: {
+				primaryColor: '#64ff00',
+				radish: '#c75c38',
+				darkPinkColor: '#bf4f74',
+				greenBlueColor: '#42b0bd',
+				lightBlueColor: '#1ecbe1',
+				greenBlueCompliment: '#bd4f42',
+				grimace: '#a660c5',
+				cosmicLatte: '#F7F3E3',
+				blush: '#E54F6D',
+				burntOrange: '#E07A5F',
+				PGray: '#666A86',
+				oldRose: '#CD8987',
+				teaGreen: '#D3EFBD',
+				teaRose: '#DEBAC0',
+				munsell: '#348AA7',
+				biceBlue: '#006DAA',
+			},
 			backgroundImage: {
 				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
 				'gradient-conic':
@@ -14,5 +32,4 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [],
 };
