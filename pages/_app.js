8252b2c59b2f919bbdd5dddef3d6828b0632af93
DSmith575 <SMITDE5@student.op.ac.nz>
2023-08-26 23:20:56 +1200

    build(component): added font and ran prettier on code (adjusted spaces to 2) emptied index.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import '@/styles/globals.css';
+import { Roboto_Mono } from 'next/font/google';
+
+const roboto = Roboto_Mono({
+  weight: '200',
+  subsets: ['greek'],
+});
 
 export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+  return (
+    <>
+      <main className={roboto.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./app/**/*.{js,ts,jsx,tsx,mdx}',
-	],
-	theme: {
-		extend: {
-			colors: {
-				primaryColor: '#64ff00',
-				radish: '#c75c38',
-				darkPinkColor: '#bf4f74',
-				greenBlueColor: '#42b0bd',
-				lightBlueColor: '#1ecbe1',
-				greenBlueCompliment: '#bd4f42',
-				grimace: '#a660c5',
-				cosmicLatte: '#F7F3E3',
-				blush: '#E54F6D',
-				burntOrange: '#E07A5F',
-				PGray: '#666A86',
-				oldRose: '#CD8987',
-				teaGreen: '#D3EFBD',
-				teaRose: '#DEBAC0',
-				munsell: '#348AA7',
-				biceBlue: '#006DAA',
-			},
-			backgroundImage: {
-				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-				'gradient-conic':
-					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-			},
-		},
-	},
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: '#64ff00',
+        radish: '#c75c38',
+        darkPinkColor: '#bf4f74',
+        greenBlueColor: '#42b0bd',
+        lightBlueColor: '#1ecbe1',
+        greenBlueCompliment: '#bd4f42',
+        grimace: '#a660c5',
+        cosmicLatte: '#F7F3E3',
+        blush: '#E54F6D',
+        burntOrange: '#E07A5F',
+        PGray: '#666A86',
+        oldRose: '#CD8987',
+        teaGreen: '#D3EFBD',
+        teaRose: '#DEBAC0',
+        munsell: '#348AA7',
+        biceBlue: '#006DAA',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
 };
