2ff1346596f2d3e9cecaa3ccac205a39c04603f5
Deacon Smith (1000106517) <SMITDE5@student.op.ac.nz>
2023-08-28 14:49:05 +1200

    build(component): adjusted card styling, added bestStories and id page

--- components/card/card.jsx
+++ components/card/card.jsx
@@ -17,7 +17,7 @@ const Card = ({ story }) => {
           'w-[85%] md:w-[55%] mx-auto p-12 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100'
         }>
         <ul className={''}>
-          <li className={'font-bold'}>{story.title}</li>
+          <li className={'font-bold text-center'}>{story.title}</li>
           <li>
             <span className={'font-bold'}>Type: </span> {story.type}
           </li>
@@ -32,16 +32,23 @@ const Card = ({ story }) => {
           <li>
             <span className={'font-bold'}>Score:</span> {story.score}
           </li>
-          <li>{story.text}</li>
+          {story.text ? (
+            <>
+              <h1 className={'font-bold mt-5'}>Story</h1>
+              <li>{story.text}</li>
+            </>
+          ) : (
+            ''
+          )}
         </ul>
-        <ul className={''}>
-          <h className={'border border-stone-900'}>Related</h>
+        <div className={'mt-5 text-center ring rounded'}>
+          <h1 className={'font-bold text-center pb-5'}>Sub Stories</h1>
           {/* Returns related stories from api.kids slices and returns the first 5 */}
           {story.kids ? (
             story.kids.slice(0, 5).map((index) => (
               <Link
                 className={
-                  'text-greenBlueColor flex flex-col font-semibold hover:font-extrabold hover:text-greenBlueColor'
+                  'text-greenBlueColor flex flex-col font-semibold hover:font-bold hover:underline hover:text-oldRose'
                 }
                 key={index}
                 href={`https://hacker-news.firebaseio.com/v0/item/${index}.json?print=pretty`}
@@ -52,21 +59,25 @@ const Card = ({ story }) => {
           ) : (
             <li>No external links</li>
           )}
-
-          {/* Returns story links if API returns any urls */}
-          {story.url ? (
-            <>
-              <div className={'border border-stone-900 mx-auto '}>
-                <h1>Story Link</h1>
-                <Link href={story.url} target={'_blank'}>
-                  URL
-                </Link>
-              </div>
-            </>
-          ) : (
-            ''
-          )}
-        </ul>
+        </div>
+        {/* Returns story links if API returns any urls */}
+        {story.url ? (
+          <>
+            <div className={'mt-5 ring rounded text-center'}>
+              <h1 className={'font-bold text-center pb-5'}>Story Link</h1>
+              <Link
+                className={
+                  'text-greenBlueColor font-bold hover:text-oldRose hover:underline'
+                }
+                href={story.url}
+                target={'_blank'}>
+                {story.url}
+              </Link>
+            </div>
+          </>
+        ) : (
+          ''
+        )}
       </div>
     </>
   );
---
+++ pages/bestStories/[id].jsx
@@ -0,0 +1,36 @@
+import Card from '@/components/card/card.jsx';
+import axios from 'axios';
+import MetaTag from '@/components/metaTag/metaTag.jsx';
+
+const BestStoryPage = ({ story }) => {
+  return (
+    <>
+      <MetaTag pageTitle={story.title} />
+      <Card story={story} />;
+    </>
+  );
+};
+
+export const getServerSideProps = async (context) => {
+  const id = context.params.id;
+
+  const api = `https://hacker-news.firebaseio.com/v0/item/${id}/.json?print=pretty`;
+  try {
+    const response = await axios.get(api);
+
+    return {
+      props: {
+        story: response.data,
+      },
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      props: {
+        story: null,
+      },
+    };
+  }
+};
+
+export default BestStoryPage;
