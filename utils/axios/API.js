78f83f1458d030e732d8343c4d626588239d416b
Deacon Smith (1000106517) <SMITDE5@student.op.ac.nz>
2023-09-05 12:32:30 +1200

    build(envimport): testing next_public

--- utils/axios/API.js
+++ utils/axios/API.js
@@ -14,7 +14,7 @@
 
 import axios from 'axios';
 
-const HACKER_NEWS_URL = process.env.HACKER_NEWS_URL;
+const HACKER_NEWS_URL = process.env.NEXT_PUBLIC_HACKER_NEWS_URL;
 
 export const storyInstance = axios.create({
   baseURL: HACKER_NEWS_URL,
